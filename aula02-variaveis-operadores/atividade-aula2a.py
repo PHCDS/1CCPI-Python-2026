cb2225b6ef8ffc77684ca0d0f4c6a83543bc5473
PHCDS <pedroc.santos2018@gmail.com>
2026-03-16 11:41:00 -0300

    aula de variaveis

---
+++ aula02-variaveis-operadores/atividade-aula2a.py
@@ -0,0 +1,9 @@
+nome = input('digite seu nome')
+dia = input('digite dia de nascimento')
+mes = input('digite mes de nascimento')
+ano = input('digite ano de nascimento')
+print(f'Bem Vindo, {nome,}{dia}/{mes}/{ano} ')
+
+n1 = int (input('digite outro numero'))
+n2 = int (input('digite outro numeto'))
+print(f'a soma destes numeros é {n1 + n2}')
---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,19 @@
+print('hello world')
+print(7 + 4)
+print('7' + '4') # estou concatenando strigs
+
+'''concatenando variaveis de multiplas linhas'''
+#variaveis
+
+nome = "Pedro"
+idade = 21
+peso = 66
+
+print(nome, idade, peso)
+print(f'ola {nome}')
+
+nome = input('digite seu nome')
+idade = input('digite sua idade')
+peso = input('digite seu peso')
+
+print(f'oi {nome, idade, peso}')
